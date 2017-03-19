81cb7f3ae4a4ec7e6ee38a5f14dad053d7908952
allejok96 <allejok96@gmail.com>
2017-03-19 19:53:09 +0100

    Local streaming, fix youtube-dl problem

--- README.md
+++ README.md
@@ -1,27 +1,21 @@
 # Send to Kodi
 
-This is a very simple script that sends a video/sound/image URL from your desktop to Kodi.
-
 ![Screenshot of dialog box](https://cloud.githubusercontent.com/assets/7693838/16900025/29b53d9c-4c18-11e6-8a74-e6d88c33e503.png)
 
-* Send video links to Kodi from your desktop.
-* No need to install the relevant add-ons in Kodi (unless it's Youtube).
-* `youtube-dl` supports sites that may not have an Kodi add-on.
-* No support for local files, though.
-
-**Note:** The youtube-dl support is very limited. It will only work if there only is a SINGLE video stream. I'm just too lazy to figure out how to fix this. If you know, please let me know...
+* Paste a video URL to play it on Kodi.
+* No Kodi add-ons required (except for Youtube).
+* `youtube-dl` supports a bunch of sites.
+* Stream local files from computer to Kodi.
 
 ## Usage
 
 1. In Kodi, enable *System > Servicies > Web server > Allow remote control via HTTP*.
 
-1. Edit the script and add your `host` and `port` in the head of the script, and optionally `user` and `pass`.
+1. Add `host` and `port` to the head of the script, and optionally `user` and `pass`.
 
-1. Run the script, either via the command line:
+1. Run the script with no arguments for a GUI, or from the command line
 
         ./send-to-kodi.sh https://vimeo.com/174312494
-
-1. Or run the script without arguments to get a GUI.
 
 ## Requirements
 
@@ -29,7 +23,10 @@ This is a very simple script that sends a video/sound/image URL from your deskto
 - zenity (for GUI)
 - Youtube add-on in Kodi (youtube support)
 - youtube-dl (for sites other than youtube, ironically)
+- netcat (local streaming)
 
-## Also check out
+## Inspired by
+
 - [Firefox extension](https://addons.mozilla.org/en-US/firefox/addon/send-to-xbmc/) supports Youtube and common file formats.
 - [Chrome extension](https://chrome.google.com/webstore/detail/play-to-kodi/fncjhcjfnnooidlkijollckpakkebden?hl=en) supports Youtube, Twitch.tv, Hulu, SVTPlay.se, SoundCloud, and much more (requires the relevant addons to be installed in Kodi)
+- [@facmachado's send2kodi script](https://github.com/facmachado/send2kodi)
--- send-to-kodi.sh
+++ send-to-kodi.sh
@@ -3,9 +3,16 @@
 # Required settings
 host=
 port=
-# Optional
+
+# Optional login for Kodi
 #user=
 #pass=
+
+# Settings for netcat (local file)
+local_hostname=$(hostname)
+local_port=12345
+
+
 
 show_help()
 {
@@ -18,60 +25,84 @@ If no URL is given, a dialog window is shown (requires zenity).
 Supports:
 Common file formats (mp4,flv,mp3,jpg and more)
 Youtube (requires the Youtube plugin in Kodi)
+Local media streaming (via netcat)
 Manny more sites (requires youtube-dl)
 
 Configuration is done in the head of the script.
-
-Note about youtube-dl support:
-If youtube-dl returns more the ONE URL this will fail. Why? Because
-I'm too lazy and dumb to figure out how to make it work. So, shame
-on me!
 
 EOF
 }
 
 error()
 {
-    type zenity &>/dev/null && zenity zenity --error --text "$*" || echo "$*" 1>&2
+    if type zenity &>/dev/null; then
+     	zenity --error --ellipsize --text "$*"
+    else
+	echo "$*" 1>&2
+    fi
+    
     exit 1
 }
 
 send_json()
 {
-    curl ${user:+--user "$user:$pass"} -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"Player.Open","params":{"item":{"file":"'"$1"'"}},"id":1}' http://$host:$port/jsonrpc || error "Failed to send link - is Kodi running?"
+    curl \
+	${user:+--user "$user:$pass"} \
+	-X POST \
+	-H "Content-Type: application/json" \
+	-d '{"jsonrpc":"2.0","method":"Player.Open","params":{"item":{"file":"'"$1"'"}},"id":1}' \
+	http://$host:$port/jsonrpc \
+	|| error "Failed to send link - is Kodi running?"
 }
 
+ytplugin='plugin://plugin.video.youtube/?action=play_video&videoid='
+
+[[ $host && $port ]] || error "Please set host and port in configuration"
 [[ "$1" = --help ]] && show_help
 
-url="$1"
-
-until [[ $url ]]; do
-    url="$(zenity --entry --title "Send to Kodi" --text "Paste an URL to a video here")" || exit
+# Dialog box?
+input="$1"
+until [[ $input ]]; do
+    input="$(zenity --entry --title "Send to Kodi" --text "Paste a video link here")" || exit
 done
 
-# youtube.com
-if [[ $url =~ ^https?://(www\.)?youtube\.com/watch\?v= ]]; then
-    send_json "plugin://plugin.video.youtube/?action=play_video&videoid=$(sed 's/.*[&?]v=\([a-zA-Z0-9]\+\).*/\1/' <<< "$url")"
-
-# youtu.be
-elif [[ $url =~ ^https?://youtu\.be/[a-zA-Z0-9] ]]; then
-    send_json "plugin://plugin.video.youtube/?action=play_video&videoid=$(sed 's/^https\?:\/\/youtu\.be\/\([a-zA-Z0-9]\+\).*/\1/' <<< "$url")"
-
-# playable files
-elif [[ $url =~ \.(mp4|mkv|mov|avi|flv|wmv|asf|mp3|flac|mka|m4a|aac|ogg|pls|jpg|png|gif|jpeg|tiff)(\?.*)?$ ]]; then
-     send_json "$url"
-
-# check URL with youtube-dl
-else
-    type youtube-dl &>/dev/null || exit
-    if new_url="$(youtube-dl -g "$url")"; then
-	new_url="$(uniq <<< "$new_url")"
-	if [[ $(wc -l <<< "$new_url") -gt 1 ]]; then
-	    error "Unable to handle multiple video links (but the site seems to be supported by youtube-dl)"
-	else
-	    send_json "$new_url"
-	fi
-    else
-	error "No videos found, or site not supported by youtube-dl"
-    fi
+if [[ $input =~ ^file:// ]]; then
+    # Remove file:// and carrige return (\r) at the end
+    input="$(sed 's%^file://%%;s/\r$//' <<< "$input")"
 fi
+
+# Get URL for...
+
+# Local media
+if [[ -e $input ]]; then
+    type nc &>/dev/null || error "netcat required"
+    [[ $local_hostname && $local_port ]] || error "Please set local hostname and port in configuration"
+
+    # Start netcat in background and kill it when we exit
+    nc -lp $local_port < "$input" &
+    trap "kill $!" EXIT
+    
+    url="tcp://$local_hostname:$local_port"
+    
+# Youtube
+elif [[ $input =~ ^https?://(www\.)?youtube\.com/watch\?v= ]]; then
+    url="$yt$(sed 's/.*[&?]v=\([a-zA-Z0-9]\+\).*/\1/' <<< "$input")"
+elif [[ $input =~ ^https?://youtu\.be/[a-zA-Z0-9] ]]; then
+    url="$ytplugin$(sed 's/^https\?:\/\/youtu\.be\/\([a-zA-Z0-9]\+\).*/\1/' <<< "$input")"
+    
+# Playable formats
+elif [[ $input =~ \.(mp4|mkv|mov|avi|flv|wmv|asf|mp3|flac|mka|m4a|aac|ogg|pls|jpg|png|gif|jpeg|tiff)(\?.*)?$ ]]; then
+     url="$input"
+     
+# Youtube-dl
+else
+    type youtube-dl &>/dev/null || exit 1
+    url="$(youtube-dl -gf best "$input")" || error "No videos found, or site not supported by youtube-dl"
+fi
+
+[[ $url ]] && send_json "$url"
+
+# Wait for netcat to exit
+wait
+# Don't kill netcat
+trap - EXIT
